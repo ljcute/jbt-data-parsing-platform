11f2a21a235e62d87d27b8d5609ef1c9051c5c77
yanpan01 <gZQVfSe47diV>
2022-07-27 11:02:13 +0800

    互联网数据解析平台框架构建

---
+++ data/ms/securities/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author  : yanpan
+# @Time    : 2022/7/27 11:01
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
--- db_connection.py
+++ db_connection.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# author lbj
 # 2022/6/23 13:55
 # pip3 install DBUtils==1.3
 
 import pymysql
 from dbutils.pooled_db import PooledDB
-from utils.logs_utils import logger
 from configparser import ConfigParser
 import os
 
@@ -14,16 +12,16 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 full_path = os.path.join(base_dir, 'config/config.ini')
 
 cf = ConfigParser()
-cf.read(full_path,encoding='utf-8')
+cf.read(full_path, encoding='utf-8')
 
-host = cf.get('mysql', 'host')
-port = cf.getint('mysql', 'port')
-username = cf.get('mysql', 'username')
-password = cf.get('mysql', 'password')
-schema = cf.get('mysql', 'schema')
-charset = cf.get('mysql', 'charset')
+host_biz = cf.get('mysql-biz', 'host')
+port_biz = cf.getint('mysql-biz', 'port')
+username_biz = cf.get('mysql-biz', 'username')
+password_biz = cf.get('mysql-biz', 'password')
+schema_biz = cf.get('mysql-biz', 'schema')
+charset_biz = cf.get('mysql-biz', 'charset')
 
-global_pool = PooledDB(
+global_pool_biz = PooledDB(
     creator=pymysql,
     maxconnections=200,
     mincached=4,
@@ -32,14 +30,37 @@ global_pool = PooledDB(
     blocking=True,
     setsession=[],
     ping=5,
-    host=host,
-    port=port,
-    user=username,
-    password=password,
-    database=schema,
-    charset=charset)
+    host=host_biz,
+    port=port_biz,
+    user=username_biz,
+    password=password_biz,
+    database=schema_biz,
+    charset=charset_biz)
+
+host_raw = cf.get('mysql-raw', 'host')
+port_raw = cf.getint('mysql-raw', 'port')
+username_raw = cf.get('mysql-raw', 'username')
+password_raw = cf.get('mysql-raw', 'password')
+schema_raw = cf.get('mysql-raw', 'schema')
+charset_raw = cf.get('mysql-raw', 'charset')
+
+global_pool_raw = PooledDB(
+    creator=pymysql,
+    maxconnections=200,
+    mincached=4,
+    maxcached=20,
+    maxshared=0,
+    blocking=True,
+    setsession=[],
+    ping=5,
+    host=host_raw,
+    port=port_raw,
+    user=username_raw,
+    password=password_raw,
+    database=schema_raw,
+    charset=charset_raw)
 
 if __name__ == '__main__':
-    conn = global_pool.connection()
+    conn = global_pool_raw.connection()
     cursor = conn.cursor()
     print(cursor)
