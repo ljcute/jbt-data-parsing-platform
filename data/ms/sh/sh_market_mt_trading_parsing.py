c37fade692b7aec84c40fee016e0cc579dd2829b
yanpan01 <gZQVfSe47diV>
2022-09-19 19:33:24 +0800

    交易所数据增加折算率比例

--- data/ms/sh/sh_market_mt_trading_parsing.py
+++ data/ms/sh/sh_market_mt_trading_parsing.py
@@ -7,6 +7,8 @@
 # @Software: PyCharm
 from data.ms.genralhandler import *
 
+factor_request_url = cf.get('factor-url', 'url')
+
 
 def sh_parsing_data(rs, data_):
     temp_list = []
@@ -23,16 +25,104 @@ def sh_parsing_data(rs, data_):
     query_result = post_data_job(query_datas)
     sec_type_list = query_result['data']
     args = []
+    actual_date = rs[1]
     for temp_data in data_:
         for sec in sec_type_list:
             if temp_data[1] == sec['sec_code_market']:
-                temp_data.append(sec['sec_id'])
+                secu_id = sec['sec_id']
+                temp_data.append(secu_id)
                 secu_type = get_secu_type(sec['sec_category'])
                 temp_data.append(secu_type)
 
         logger.info(temp_data)
+    if rs[2] == '2':
+        # 担保券
+        stock_temp_list = []
+        bond_temp_list = []
+        fund_temp_list = []
+        for i in data_:
+            if i[4] == 'stock':
+                stock_temp_list.append([i[3]])
+            elif i[4] == 'bond':
+                bond_temp_list.append([i[3]])
+            elif i[4] == 'fund':
+                fund_temp_list.append([i[3]])
 
+        stock_query_data = {
+            "module": "server.fdb.factor.api.factor_api",
+            "method": "get_factor_data_cube",
+            "kwargs": {
+                "dt": str(rs[1]),
+                "obj_type": "stock",
+                "object_ids": stock_temp_list,
+                "factors": "s_ex_guaranty_limit_rate"
+            }
+        }
+        stock_res_list = query_normal_rate(stock_query_data)['data']['data']
+        for b in data_:
+            for res in stock_res_list:
+                if str(b[3]) == res[0]:
+                    b.append(res[1])
+
+        bond_query_data = {
+            "module": "server.fdb.factor.api.factor_api",
+            "method": "get_factor_data_cube",
+            "kwargs": {
+                "dt": str(rs[1]),
+                "obj_type": "bond",
+                "object_ids": bond_temp_list,
+                "factors": "b_ex_guaranty_limit_rate"
+            }
+        }
+        bond_res_list = query_normal_rate(bond_query_data)['data']['data']
+        for bb in data_:
+            for res in bond_res_list:
+                if str(bb[3]) == res[0]:
+                    bb.append(res[1])
+
+        fund_query_data = {
+            "module": "server.fdb.factor.api.factor_api",
+            "method": "get_factor_data_cube",
+            "kwargs": {
+                "dt": str(rs[1]),
+                "obj_type": "fund",
+                "object_ids": fund_temp_list,
+                "factors": "f_ex_guaranty_limit_rate"
+            }
+        }
+        fund_res_list = query_normal_rate(fund_query_data)['data']['data']
+        for bbb in data_:
+            for res in fund_res_list:
+                if str(bbb[3]) == res[0]:
+                    bbb.append(res[1])
+
+        a = []
+        b = []
+        for bb in data_:
+            if len(bb) == 5:
+                a.append(bb)
+            elif len(bb) == 6:
+                b.append(bb)
+
+        logger.error(f'如下数据无法通过全量因子库查询到对应折算率上限:{a}')
     data_parsing(rs, data_)
+
+
+# 调用全量因子库接口
+def query_normal_rate(data):
+    url = factor_request_url + '/api/gateway'
+    res = requests.post(url=url, json=data)
+    if res.status_code != 200:
+        logger.error(f'请求服务异常，uri={url}，json={data}，response={res.text}', exc_info=True)
+        raise Exception(res.text)
+    if res.text:
+        try:
+            return res.json()
+        except Exception as ex:
+            logger.error(f'请求服务异常，uri={url}，json={data}，response={res.text}，error={ex}', exc_info=True)
+            raise ex
+    else:
+        return None
 
 
 def data_parsing(rs, data_):
@@ -55,13 +145,28 @@ def data_parsing(rs, data_):
         # 查询结果为空，第一次处理，从数据采集平台爬取到的数据进行入库处理,调整类型为调入
         logger.info(f'进入为空的判断...')
         insert_data_list = []
-        for i in data_:
-            if len(i) == 5:
-                insert_data_list.append([broker_id, None if i[3] == '-' else i[3], i[4], biz_type, adjust_status_in, None, None, 1, 1, rs[1],
-                                         forever_end_dt, 1])
-            else:
-                logger.error(f'该条数据无证券id，请检查!{i}')
-                invalid_data_list.append(i)
+        if biz_type == 3:
+            for i in data_:
+                if len(i) == 5:
+                    insert_data_list.append([broker_id, None if i[3] == '-' else i[3], i[4], biz_type, adjust_status_in, None, None, 1, 1, rs[1],
+                                             forever_end_dt, 1])
+                # else:
+                #     logger.error(f'该条数据无证券id，请检查!{i}')
+                #     invalid_data_list.append(i)
+
+                if len(i) == 6:
+                    insert_data_list.append([broker_id, None if i[3] == '-' else i[3], i[4], biz_type, adjust_status_in, None, i[5], 1, 1, rs[1],
+                         forever_end_dt, 1])
+        elif biz_type == 1:
+            for i in data_:
+                if len(i) == 5:
+                    insert_data_list.append([broker_id, None if i[3] == '-' else i[3], i[4], biz_type, adjust_status_in, None, 100, 1, 1, rs[1],
+                                             forever_end_dt, 1])
+        elif biz_type == 2:
+            for i in data_:
+                if len(i) == 5:
+                    insert_data_list.append([broker_id, None if i[3] == '-' else i[3], i[4], biz_type, adjust_status_in, None, 50, 1, 1, rs[1],
+                                             forever_end_dt, 1])
         if insert_data_list:
             logger.info(f'上海交易所业务数据入库开始...')
             insert_broker_mt_business_security(insert_data_list)
@@ -73,7 +178,7 @@ def data_parsing(rs, data_):
         for col in result.values.tolist():
             haved_list.append(col[2])
         for row in data_:
-            if len(row) == 5:
+            if len(row) == 5 or len(row) == 6:
                 query_list.append(row[3])
         s_list = list(set(query_list).difference(set(haved_list)))
         if s_list:
@@ -82,7 +187,7 @@ def data_parsing(rs, data_):
                 for temp_data in data_:
                     if b == temp_data[3]:
                         secu_type = temp_data[4]
-                        temp_insert_data_list.append([broker_id, None if b == '-' else b, secu_type, biz_type, adjust_status_in, None, None, 1, 1, datetime.datetime.now(), forever_end_dt, 1])
+                        temp_insert_data_list.append([broker_id, None if b == '-' else b, secu_type, biz_type, adjust_status_in, None, temp_data[5] if len(temp_data) == 6 else None, 1, 1, datetime.datetime.now(), forever_end_dt, 1])
             logger.info(f'上海交易所业务数据入库开始...')
             insert_broker_mt_business_security(temp_insert_data_list)
             logger.info(f'上海交易所业务数据入库完成，共{len(temp_insert_data_list)}条')
--- data/ms/sz/sz_market_mt_trading_parsing.py
+++ data/ms/sz/sz_market_mt_trading_parsing.py
@@ -6,6 +6,7 @@
 # @File    : sz_market_mt_trading_parsing.py
 # @Software: PyCharm
 from data.ms.genralhandler import *
+from data.ms.sh.sh_market_mt_trading_parsing import query_normal_rate
 
 
 def sz_parsing_data(rs, data_):
@@ -27,15 +28,85 @@ def sz_parsing_data(rs, data_):
     query_result = post_data_job(query_datas)
     sec_type_list = query_result['data']
     args = []
+    actual_date = rs[1]
     for temp_data in new_data:
         for sec in sec_type_list:
             if temp_data[0] == sec['sec_code_market']:
-                temp_data.append(sec['sec_id'])
+                secu_id = sec['sec_id']
+                temp_data.append(secu_id)
                 secu_type = get_secu_type(sec['sec_category'])
                 temp_data.append(secu_type)
 
         logger.info(temp_data)
+    if rs[2] == '2':
+        stock_temp_list = []
+        bond_temp_list = []
+        fund_temp_list = []
+        for i in new_data:
+            if i[3] == 'stock':
+                stock_temp_list.append([i[2]])
+            elif i[3] == 'bond':
+                bond_temp_list.append([i[2]])
+            elif i[3] == 'fund':
+                fund_temp_list.append([i[2]])
 
+        stock_query_data = {
+            "module": "server.fdb.factor.api.factor_api",
+            "method": "get_factor_data_cube",
+            "kwargs": {
+                "dt": str(rs[1]),
+                "obj_type": "stock",
+                "object_ids": stock_temp_list,
+                "factors": "s_ex_guaranty_limit_rate"
+            }
+        }
+        stock_res_list = query_normal_rate(stock_query_data)['data']['data']
+        for b in new_data:
+            for res in stock_res_list:
+                if str(b[2]) == res[0]:
+                    b.append(res[1])
+
+        bond_query_data = {
+            "module": "server.fdb.factor.api.factor_api",
+            "method": "get_factor_data_cube",
+            "kwargs": {
+                "dt": str(rs[1]),
+                "obj_type": "bond",
+                "object_ids": bond_temp_list,
+                "factors": "b_ex_guaranty_limit_rate"
+            }
+        }
+        bond_res_list = query_normal_rate(bond_query_data)['data']['data']
+        for bb in new_data:
+            for res in bond_res_list:
+                if str(bb[2]) == res[0]:
+                    bb.append(res[1])
+
+        fund_query_data = {
+            "module": "server.fdb.factor.api.factor_api",
+            "method": "get_factor_data_cube",
+            "kwargs": {
+                "dt": str(rs[1]),
+                "obj_type": "fund",
+                "object_ids": fund_temp_list,
+                "factors": "f_ex_guaranty_limit_rate"
+            }
+        }
+        fund_res_list = query_normal_rate(fund_query_data)['data']['data']
+        for bbb in new_data:
+            for res in fund_res_list:
+                if str(bbb[2]) == res[0]:
+                    bbb.append(res[1])
+
+        a = []
+        b = []
+        for bb in new_data:
+            if len(bb) == 4:
+                a.append(bb)
+            elif len(bb) == 5:
+                b.append(bb)
+
+        logger.error(f'如下数据无法通过全量因子库查询到对应折算率上限:{a}')
 
     if rs[2] == '2':
         logger.info(f'担保券证券解析')
@@ -60,14 +131,32 @@ def sz_data_parsing(rs, biz_type, data_):
         # 查询结果为空，第一次处理，从数据采集平台爬取到的数据进行入库处理,调整类型为调入
         logger.info(f'进入为空的判断...')
         insert_data_list = []
-        for i in data_:
-            if len(i) == 4:
-                insert_data_list.append([broker_id, None if i[2] == '-' else i[2], i[3], biz_type, adjust_status_in, None, None, 1, 1, rs[1],
-                                         forever_end_dt, 2])
-            else:
-                logger.error(f'该条数据无证券id，请检查!{i}')
-                invalid_data_list.append(i)
-                raise Exception(f'该条数据无证券id，请检查!{i}')
+        if biz_type == 3:
+            for i in data_:
+                if len(i) == 4:
+                    insert_data_list.append([broker_id, None if i[2] == '-' else i[2], i[3], biz_type, adjust_status_in, None, None, 1, 1, rs[1],
+                                             forever_end_dt, 2])
+                # else:
+                #     logger.error(f'该条数据无证券id，请检查!{i}')
+                #     invalid_data_list.append(i)
+                #     raise Exception(f'该条数据无证券id，请检查!{i}')
+                if len(i) == 5:
+                    insert_data_list.append([broker_id, None if i[2] == '-' else i[2], i[3], biz_type, adjust_status_in, None, i[4], 1, 1, rs[1],
+                                             forever_end_dt, 2])
+        elif biz_type == 1:
+            for i in data_:
+                if len(i) == 4:
+                    insert_data_list.append(
+                        [broker_id, None if i[2] == '-' else i[2], i[3], biz_type, adjust_status_in, None, 100, 1, 1,
+                         rs[1],
+                         forever_end_dt, 2])
+        elif biz_type == 2:
+            for i in data_:
+                if len(i) == 4:
+                    insert_data_list.append(
+                        [broker_id, None if i[2] == '-' else i[2], i[3], biz_type, adjust_status_in, None, 50, 1, 1,
+                         rs[1],
+                         forever_end_dt, 2])
         if insert_data_list:
             logger.info(f'深圳交易所业务数据入库开始...')
             insert_broker_mt_business_security(insert_data_list)
@@ -81,7 +170,7 @@ def sz_data_parsing(rs, biz_type, data_):
             haved_list.append(col[2])
 
         for row in data_:
-            if len(row) == 4:
+            if len(row) == 4 or len(row) == 5:
                 query_list.append(row[2])
 
         s_list = list(set(query_list).difference(set(haved_list)))
@@ -91,7 +180,7 @@ def sz_data_parsing(rs, biz_type, data_):
                 for temp_data in data_:
                     if b == temp_data[2]:
                         secu_type = temp_data[3]
-                        temp_insert_data_list.append([broker_id, None if b == '-' else b, secu_type, biz_type, adjust_status_in, None, None, 1, 1,
+                        temp_insert_data_list.append([broker_id, None if b == '-' else b, secu_type, biz_type, adjust_status_in, None, temp_data[4] if len(temp_data)== 5 else None, 1, 1,
                                                       datetime.datetime.now(), forever_end_dt, 2])
             logger.info(f'深圳交易所业务数据入库开始...')
             insert_broker_mt_business_security(temp_insert_data_list)
