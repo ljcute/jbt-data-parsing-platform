116ea3af050f6c4d7ef3b1c696353eb888e98e54
Eagle <liuzh@igoldenbeta.com>
2023-02-13 16:54:29 +0800

    新增申万宏源、中信建投北交所担保品和标的券解析入库

--- data/ms/securities/swhy.py
+++ data/ms/securities/swhy.py
@@ -12,7 +12,7 @@ from data.ms.base_tools import code_ref_id, get_df_from_cdata
 
 def _get_format_df(cdata):
     df = get_df_from_cdata(cdata)
-    df['market'] = df['市场'].map(lambda x: 'SZ' if x == '深圳' else 'SH' if x == '上海' else 'BJ' if x == '北京' else x)
+    df['market'] = df['市场'].map(lambda x: 'SZ' if x == '深圳' else 'SH' if x == '上海' else 'BJ' if x in ('北京', 'B') else x)
     df['sec_code'] = df['证券代码'].apply(lambda x: ('000000'+str(x))[-max(6, len(str(x))):])
     df['sec_code'] = df['sec_code'] + '.' + df['market']
     df['sec_name'] = df['证券简称']
