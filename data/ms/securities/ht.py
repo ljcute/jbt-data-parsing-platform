fb3037bcecab5b213b72a8fdb230b6c428d6ac3b
Eagle <liuzh@igoldenbeta.com>
2023-02-14 16:09:12 +0800

    新增华泰证券北交所担保品和标的券解析入库

--- data/ms/securities/ht.py
+++ data/ms/securities/ht.py
@@ -12,7 +12,7 @@ from data.ms.base_tools import code_ref_id, get_df_from_cdata
 
 def _get_format_df(cdata):
     df = get_df_from_cdata(cdata)
-    df['market'] = df['exchangeType'].map(lambda x: 'SZ' if int(x) == 2 else 'SH' if int(x) == 1 else 'BJ' if int(x) == 3 else str(x))
+    df['market'] = df['exchangeType'].map(lambda x: 'SZ' if int(x) == 2 else 'SH' if int(x) == 1 else 'BJ' if int(x) == 9 else str(x))
     df['sec_code'] = df['stockCode'].apply(lambda x: ('000000'+str(x))[-max(6, len(str(x))):])
     df['sec_code'] = df['sec_code'] + '.' + df['market']
     df['sec_name'] = df['stockName']
