2dede529e33d8e8d9a67a467819f51b82ab0b03a
yanpan01 <gZQVfSe47diV>
2023-03-03 15:02:05 +0800

    新增方正证券数据解析程序

---
+++ data/ms/securities/fz.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3.10.0
+# -*- coding: utf-8 -*-
+"""
+@Description :
+@Date        : 2023-03-03
+@Author      : yanpan
+@Software    : PyCharm
+"""
+import pandas as pd
+from data.ms.base_tools import match_sid_by_code_and_name, get_df_from_cdata
+
+
+def _get_format_df(cdata):
+    df = get_df_from_cdata(cdata)
+    df['sec_code'] = df['stockCode'].apply(lambda x: ('000000'+str(x))[-max(6, len(str(x))):])
+    df['sec_name'] = df['stockName'].str.replace(' ', '')
+    _df = match_sid_by_code_and_name(df)
+    df = df.merge(_df, on=['sec_code', 'sec_name'])
+    df['sec_code'] = df['scd']
+    df['start_dt'] = None
+    biz_dt = df['tradeDate'].values[0]
+    return biz_dt, df
+
+
+def _format_dbq(cdata, market):
+    biz_dt, df = _get_format_df(cdata)
+    df['rate'] = df['rate'].apply(lambda x: int(x * 100))
+    dbq = df[['sec_type', 'sec_id', 'sec_code', 'rate']].copy()
+    return biz_dt, dbq, pd.DataFrame()
+
+
+def _format_rz_rq_bdq(cdata, market):
+    biz_dt, df = _get_format_df(cdata)
+    df['rz_rate'] = df['finRate'].apply(lambda x: int(x * 100))
+    df['rq_rate'] = df['saleRate'].apply(lambda x: int(x * 100))
+    rz = df[['sec_type', 'sec_id', 'sec_code', 'rz_rate']].copy()
+    rz.rename(columns={'rz_rate': 'rate'}, inplace=True)
+    rq = df[['sec_type', 'sec_id', 'sec_code', 'rq_rate']].copy()
+    rq.rename(columns={'rq_rate': 'rate'}, inplace=True)
+    rz = rz[rz['rate'] >= 100]
+    rq = rq[rq['rate'] >= 50]
+    return biz_dt, rz, rq
